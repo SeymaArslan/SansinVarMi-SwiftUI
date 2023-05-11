476c77445a1316bf362147f4e284768ee9b8b2a8
SeymaArslan <seymarslannn@gmail.com>
2023-05-11 13:49:05 +0300

    Initial Commit

---
+++ SansinVarMi-SwiftUI/SansinVarMi_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SansinVarMi_SwiftUIApp.swift
+//  SansinVarMi-SwiftUI
+//
+//  Created by Seyma on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SansinVarMi_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
